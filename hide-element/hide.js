4053413db1b03e1cb4e59c6dc2238e2524c98183
blaz-k <blazyy@gmail.com>
2021-08-16 13:46:23 +0200

    when pressed, this.style.visibility="hidden"

---
+++ hide-element/hide.js
@@ -0,0 +1,9 @@
+
+let blueButton = document.getElementById("blueButton");
+
+blueButton.addEventListener("click", function() {
+    let mainHeadingId = document.getElementById("hello");
+    mainHeadingId.classList.add("blue-text");
+
+    this.style.visibility="hidden"
+});
