a6f6c517ab26e2a0a82f9a24401126798621eced
blaz-k <blazyy@gmail.com>
2021-08-17 12:31:51 +0200

    no message

---
+++ lottery/lottery.js
@@ -0,0 +1,17 @@
+
+(function(){
+    console.log("Lottery");
+
+    let result = document.getElementById("generateNumbersButton");
+    result.addEventListener("click", function(){
+        let firstNumber = parseInt(document.getElementById("firstNumber").value);
+        let lastNumber = parseInt(document.getElementById("lastNumber").value);
+        let howManyNumbers = parseInt(document.getElementById("howManyNumbers").value);
+        let numArray = [];
+
+        for (let i=0; numArray.length < howManyNumbers; i++){
+
+        }
+    })
+
+}())
