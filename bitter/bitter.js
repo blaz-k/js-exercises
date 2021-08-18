160b7d754593b3be5e45c763b16eb37104e83271
blaz <blazyy@gmail.com>
2021-08-18 12:31:34 +0200

    Bitter created

---
+++ bitter/bitter.js
@@ -0,0 +1,62 @@
+console.log("Bitter")
+
+let container = document.getElementById("container")
+
+let xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4) {
+        if (this.status === 200) {
+            let bitts = JSON.parse(xhttp.responseText);
+
+            for ( let i of bitts){
+                let bitElement = document.createElement("p");
+                bitElement.innerHTML = i.username + " " + i.created + " - " + i.text;
+                container.appendChild(bitElement);
+            }
+
+        } else {
+        console.log("error")
+        }
+    }
+};
+
+xhttp.open("GET", "http://localhost:5000/get-all-bitts", true);
+xhttp.send();
+
+let username = document.getElementById("username");
+let text = document.getElementById("text");
+let submit = document.getElementById("submit");
+let jsonData;
+
+let xhttp2 = new XMLHttpRequest();
+
+
+submit.addEventListener("click", function() {
+    console.log("CLICK")
+
+    let usernameText = {username: username.value, text: text.value}
+    console.log(usernameText)
+    jsonData = JSON.stringify(usernameText)
+
+
+    xhttp2.onload = function(){
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                console.log(xhttp2.responseText);
+            } else {
+                console.log("ERROR");
+            }
+
+        }
+
+    }
+    xhttp2.open("POST", "http://localhost:5000/create-bitt", true )
+    xhttp2.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp2.send(jsonData)
+
+});
+
+
+
+
